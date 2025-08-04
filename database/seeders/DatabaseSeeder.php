ac0cb193f78accd56353d605e645ebca4b98222f
Akmal <akmalthemalong@gmail.com>
2025-08-04 20:28:56 +0800

    feat(database): Tambah data kecamatan dan desa riil pada seeder
    Mengganti pembuatan data kecamatan dan desa secara acak dengan daftar nama riil yang telah ditentukan. Ini memastikan konsistensi dan keakuratan data geografis saat proses seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,36 +1,47 @@
 <?php
 
-namespace Database\Seeders;
-
-use App\Enums\Role;
-use App\Models\Berita;
-use App\Models\Edukasi;
-use App\Models\Komentar;
-use App\Models\User;
-use App\Models\Desa;
 use App\Models\Kecamatan;
+use App\Models\Desa;
+use App\Models\User;
+use App\Enums\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Buat beberapa kecamatan
-        $kecamatan = \App\Models\Kecamatan::factory(3)->create();
+        // Data kecamatan dan desa
+        $wilayah = [
+            ['Baula', ['Baula', 'Longori', 'Pewutaa', 'Puubenua', 'Puubunga', 'Puulemo', 'Puundoho', 'Puuroda', 'Ulu Baula', 'Watalara']],
+            ['Iwoimendaa', ['Iwoimendaa', 'Ladahai', 'Lambopini', 'Landoula', 'Lasiroku', 'Lawolia', 'Tamborasi', 'Ulu Kalo', 'Watumelewe', 'Wonualaku']],
+            ['Kolaka', ['Balandete', 'Laloeha', 'Lalombaa', 'Lamokato', 'Sabilambo', 'Tahoa', 'Watuliandu']],
+            ['Latambaga', ['Induha', 'Kolakaasi', 'Latambaga', 'Mangolo', 'Sakuli', 'Sea', 'Ulunggolaka']],
+            ['Polinggona', ['Lamondape', 'Plasma Jaya', 'Polinggona', 'Pondowae', 'Puudongi', 'Tanggeau', 'Wulonggere']],
+            ['Pomalaa', ['Dawi-Dawi', 'Hakatutobu', 'Huko-Huko', 'Kumoro', 'Oko-Oko', 'Pelambua', 'Pesouha', 'Pomalaa', 'Sopura', 'Tambea', 'Tonggoni', 'Totobo']],
+            ['Samaturu', ['Amamutu', 'Awa', 'Kaloloa', 'Konaweha', 'Lambolemo', 'Latuo', 'Lawulo', 'Liku', 'Malaha', 'Meura', 'Puu Lawulo', 'Puu Tamboli', 'Sani-sani', 'Tamboli', 'Tonganapo', 'Tosiba', 'Ulaweng', 'Ulu Konaweha', 'Wawo Tambali']],
+            ['Tanggetada', ['Anaiwoi', 'Lalonggolosua', 'Lamedai', 'Lamoiko', 'Oneeha', 'Palewai', 'Petudua', 'Pewisoa Jaya', 'Popalia', 'Puundaipa', 'Rahanggada', 'Tanggetada', 'Tinggo', 'Tondowolio']],
+            ['Toari', ['Anawua', 'Horongkuli', 'Lakito', 'Rahabite', 'Rano Jaya', 'Rano Sangia', 'Ranomentaa', 'Toari', 'Wonua Raya', 'Wowoli']],
+            ['Watubangga', ['Gunung Sari', 'Kastura', 'Kukutio', 'Lamundre', 'Langgosipi', 'Mataosu', 'Mataosu Ujung', 'Peoho', 'Polenga', 'Ranoteta', 'Sumber Rejeki', 'Tandebura', 'Watubangga', 'Wolulu']],
+            ['Wolo', ['Donggala', 'Iwoimopuro', 'Lalonaha', 'Lalonggopi', 'Lana', 'Langgomali', 'Lapao-Pao', 'Muara Lapao-Pao', 'Samaenre', 'Tolowe Ponrewaru', 'Ulu Rina', 'Ulu Wolo', 'Ululapao-pao', 'Wolo']],
+            ['Wundulako', ['19 Nopember', 'Bende', 'Kowioha', 'Lamekongga', 'Ngapa', 'Sabiano', 'Silea', 'Tikonu', 'Towua', 'Unamendaa', 'Wundulako']],
+        ];
 
-        // Buat beberapa desa per kecamatan
         $desaList = collect();
-        $kecamatan->each(function ($kec) use (&$desaList) {
-            $desaList = $desaList->merge(
-                Desa::factory(3)->create([
-                    'id_kecamatan'=> $kec->id
-                ])
-            );
-        });
+
+        foreach ($wilayah as [$namaKecamatan, $desaDesa]) {
+            $kecamatan = Kecamatan::create(['nama' => $namaKecamatan]);
+            foreach ($desaDesa as $namaDesa) {
+                $desaList->push(
+                    Desa::create([
+                        'id_kecamatan' => $kecamatan->id,
+                        'nama' => $namaDesa
+                    ])
+                );
+            }
+        }
 
         // Ambil daftar desa secara acak untuk dipakai user
         $desaIds = $desaList->pluck('id');
-        $kecamatanIds = $kecamatan->pluck('id');
 
         // Buat user dengan id_desa secara acak
         User::factory()->create([
@@ -67,11 +78,5 @@ class DatabaseSeeder extends Seeder
             'role' => Role::KEPALADINAS->value,
             'id_desa' => $desaIds->random(),
         ]);
-
-        // Uncomment jika ingin menambahkan dummy data
-        // User::factory(10)->create();
-        // Berita::factory(5)->create();
-        // Edukasi::factory(8)->create();
-        // Komentar::factory(20)->create();
     }
 }
