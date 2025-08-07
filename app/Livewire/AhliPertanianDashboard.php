4cef141c1e5b550ea896e5b6173f9b75845d0208
Akmal <akmalthemalong@gmail.com>
2025-08-07 08:08:05 +0800

    feat(dashboard-ahli): Filter jumlah konsultasi berdasarkan kecamatan ahli

--- app/Livewire/AhliPertanianDashboard.php
+++ app/Livewire/AhliPertanianDashboard.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\Desa;
 use App\Models\Konsultasi;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class AhliPertanianDashboard extends Component
@@ -10,7 +12,17 @@ class AhliPertanianDashboard extends Component
     public function render()
     {
         return view('livewire.ahli-pertanian-dashboard', [
-            'jumlah_konsultasi' => Konsultasi::count(),
+            'jumlah_konsultasi' => Konsultasi::with(['user.desa'])
+                ->whereHas('user.desa', function($query) {
+
+                    $currentUser = Auth::user();
+                    if($currentUser && $currentUser->id_desa) {
+                        $kecamatanId = Desa::find($currentUser->id_desa)->id_kecamatan;
+                        $query->where('id_kecamatan', $kecamatanId);
+                    }
+
+                })->count()
+            ,
         ]);
     }
 }
