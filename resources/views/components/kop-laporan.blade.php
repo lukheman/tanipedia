c51cae1a9750fbd46ee8147f1bbb0b2ff9253b89
Akmal <akmalthemalong@gmail.com>
2025-08-10 11:50:37 +0800

    feat(komponen-laporan): Menambahkan komponen kop laporan

---
+++ resources/views/components/kop-laporan.blade.php
@@ -0,0 +1,5 @@
+    <div class="kop-surat">
+        <h2>Dinas Tanaman Pangan dan Hortikultura Kabupaten Kolaka</h2>
+        <!-- <p>Jl. Raya Tani No. 123, Jakarta</p> -->
+        <!-- <p>Telp: (021) 123-4567 | Email: info@dinaspertanian.jkt</p> -->
+    </div>
