59dbcf7cdc150a51184336b9b10a731e1035ca34
Akmal <akmalthemalong@gmail.com>
2025-08-11 10:48:54 +0800

    refactor(komponen): Ekstraksi blok tanda tangan menjadi komponen
    Membuat komponen Blade baru `signature` untuk blok tanda tangan.
    Mengganti implementasi tanda tangan yang terduplikasi di laporan konsultasi dan laporan pengguna dengan komponen `x-signature` yang baru.
    Peningkatan reusabilitas dan kemudahan pemeliharaan.

--- resources/views/invoices/laporan-users.blade.php
+++ resources/views/invoices/laporan-users.blade.php
@@ -41,13 +41,5 @@
     </div>
 
     <!-- Tanda Tangan -->
-    <div class="signature">
-        <div class="col"></div>
-        <div class="col">
-            <p>Dinas Tanaman Pangan <br> dan Hortikultura Kabupaten Kolaka</p>
-
-            <p class="ttd">SUYANTO, SP., M.Si </p> <br>
-            <p class="nip">NIP. 19650111 198709 1 001</p>
-        </div>
-    </div>
+    <x-signature />
 </x-laporan>
