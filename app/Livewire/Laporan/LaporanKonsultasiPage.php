1d66b17db90876dcf286db20131b32525c704a45
Akmal <akmalthemalong@gmail.com>
2025-08-11 20:24:59 +0800

    feat(laporan-konsultasi): Tambahkan fitur detail konsultasi
    Mengimplementasikan tampilan modal untuk melihat rincian lengkap konsultasi, termasuk nama petani, isi keluhan, nama tanaman, tanggal konsultasi, serta jawaban dan penanggap dari ahli pertanian. Tombol "Download" pada tabel diganti menjadi tombol "Detail" untuk mengakses fitur ini.

--- app/Livewire/Laporan/LaporanKonsultasiPage.php
+++ app/Livewire/Laporan/LaporanKonsultasiPage.php
@@ -14,6 +14,30 @@ class LaporanKonsultasiPage extends Component
 
     public $kecamatan;
 
+    public $selectedIKonsultasi;
+    public $nama_petani = '';
+    public $isi = '';
+    public $nama_tanaman = '';
+    public $tanggal_konsultasi = '';
+
+    public function detail($konsultasi)
+    {
+        $this->selectedIKonsultasi = $konsultasi['id'];
+        $this->nama_petani = $konsultasi['user']['name'];
+        $this->isi = $konsultasi['isi'];
+        $this->nama_tanaman = $konsultasi['nama_tanaman'];
+        $this->tanggal_konsultasi = $konsultasi['tanggal_konsultasi'];
+
+        $konsultasi = Konsultasi::with('hasil', 'hasil.user')->find($konsultasi['id']);
+        if ($konsultasi->hasil) {
+            $this->nama_ahli_pertanian = $konsultasi->hasil->user->name;
+            $this->jawaban = $konsultasi->hasil->isi;
+        }
+
+        $this->openModal('modal-detail-konsultasi');
+
+    }
+
     public function render()
     {
         return view('livewire.laporan.laporan-konsultasi-page', [
--- resources/views/livewire/laporan/laporan-konsultasi-page.blade.php
+++ resources/views/livewire/laporan/laporan-konsultasi-page.blade.php
@@ -1,6 +1,68 @@
 <div class="card">
     <div class="card-header">
 
+<!-- Modal Detail Konsultasi -->
+<div class="modal fade" id="modal-detail-konsultasi" tabindex="-1" aria-labelledby="myModalLabel1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+        <div class="modal-content shadow-lg rounded-3">
+            <!-- Header -->
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title text-white" id="myModalLabel1">
+                    <i class="bi bi-eye"></i> Detail Konsultasi
+                </h5>
+                <button type="button" class="close rounded-pill" wire:click="$dispatch('closeModal', {id: 'modal-detail-konsultasi'})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                </button>
+            </div>
+
+            <!-- Body -->
+            <div class="modal-body">
+                <!-- Informasi Petani -->
+                <div class="row g-3 mb-3">
+                    <div class="col-md-8">
+                        <label class="form-label fw-semibold" for="judul">Nama Petani</label>
+                        <input type="text" id="judul" wire:model="nama_petani" class="form-control-plaintext border rounded px-3 py-2 bg-white" readonly>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="form-label fw-semibold" for="tanggal-publikasi">Tanggal Konsultasi</label>
+                        <input type="text" id="tanggal-publikasi" wire:model="tanggal_konsultasi" class="form-control-plaintext border rounded px-3 py-2 bg-white" readonly>
+                    </div>
+                </div>
+
+                <!-- Detail Konsultasi -->
+                <div class="mb-4">
+                    <h6 class="fw-bold">Detail Konsultasi</h6>
+                    <div class="border rounded p-3 bg-light">
+                        <p><strong>Tanaman:</strong> {{ $nama_tanaman }}</p>
+                        <hr>
+                        <p><strong>Keluhan:</strong></p>
+                        <div class="text-dark" style="white-space: pre-line;">{{ $isi }}</div>
+                    </div>
+                </div>
+
+                <!-- Jawaban -->
+                <div class="mb-2">
+                    <h6 class="fw-bold">Jawaban Ahli Pertanian</h6>
+                    <div class="border rounded p-3 bg-light">
+
+                        <p><strong>Ditanggapi oleh:</strong> {{ $nama_ahli_pertanian ?? 'Ahli Pertanian Tidak Ada' }}</p>
+                        <hr>
+                        <div class="text-dark" style="white-space: pre-line;">
+                            {{ $jawaban ?? 'Belum ada jawaban.' }}
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
+
         <div class="modal fade text-left show" id="modal-cetak-laporan-konsultasi" tabindex="-1" role="dialog" style="display: none;">
             <div class="modal-dialog modal-dialog-scrollable" role="document">
                 <div class="modal-content">
@@ -63,14 +125,7 @@
                     <td>{{ $item->tanggal_konsultasi }}</td>
                     <td>{{ $item->nama_tanaman }}</td>
                     <td class="text-end">
-                    <form action="{{ route('print-laporan.konsultasi') }}" method="post">
-                    @csrf
-                            <input type="hidden" name="id" value="{{ $item->id }}">
-                        <button type="submit" class="btn btn-sm btn-danger" >
-
-    <i class="bi bi-printer"></i>
-                                    Download</button>
-                    </form>
+                        <button wire:click="detail({{ $item }})" class="btn btn-sm btn-info">Detail</button>
 
                     </td>
                 </tr>
