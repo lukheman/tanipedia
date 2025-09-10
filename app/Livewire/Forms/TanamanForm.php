be02b7481e4bc7a74625593e8344bc1ccf63b346
Akmal <akmalthemalong@gmail.com>
2025-09-10 19:46:01 +0800

    feat: tanaman dan video form

---
+++ app/Livewire/Forms/TanamanForm.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+use App\Models\Tanaman;
+
+class TanamanForm extends Form
+{
+
+    public ?Tanaman $tanaman = null;
+
+    public string $nama = '';
+
+    protected function rules(): array
+    {
+        return [
+            'nama' => ['required', 'string', 'max:255', Rule::unique('tanaman', 'nama')->ignore($this->tanaman)],
+        ];
+    }
+
+    protected function messages(): array {
+        return [
+            'nama.required' => 'Nama tanaman harus diisi.',
+            'nama.string' => 'Nama tanaman harus berupa teks.',
+            'nama.max' => 'Nama tanaman tidak boleh lebih dari 255 karakter.',
+            'nama.unique' => 'Nama tanaman sudah ada.',
+        ];
+    }
+
+    public function store() {
+        $this->validate();
+
+        Tanaman::query()->create([
+            'nama' => $this->nama,
+        ]);
+
+        $this->reset();
+
+    }
+
+    public function update() {
+        $this->validate();
+        $this->tanaman->update([
+            'nama' => $this->nama,
+        ]);
+
+    }
+
+    public function delete() {
+        $this->tanaman->delete();
+        $this->reset();
+    }
+
+
+    public function fillFromModel(Tanaman $tanaman) {
+        $this->tanaman = $tanaman;
+        $this->nama = $tanaman->nama;
+    }
+
+}
---
+++ app/Livewire/Forms/VideoForm.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use App\Models\Edukasi;
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class VideoForm extends Form
+{
+    public ?Edukasi $edukasi = null;
+
+    public $judul;
+
+    public $deskripsi;
+
+    public $url_video;
+
+    public $video;
+
+    protected function rules(): array
+    {
+        return [
+            'judul' => 'required|string|min:3|max:255',
+            'deskripsi' => 'string|min:3|max:255',
+            'video' => 'nullable|file|mimes:mp4,avi,mpeg,mov|max:512000',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'judul.required' => 'Judul wajib diisi',
+            'judul.min' => 'Judul minimal 3 karakter',
+            'judul.max' => 'Judul maksimal 255 karakter',
+            'deskripsi.min' => 'Deskripsi minimal 3 karakter',
+            'deskripsi.max' => 'Deskripsi maksimal 255 karakter',
+            'video.required' => 'Video wajib diisi',
+            'video.mimes' => 'Format video harus mp4, avi, mpeg, atau mov',
+            'video.max' => 'Ukuran video maksimal 100MB',
+        ];
+    }
+
+    public function store() {
+        $this->validate();
+
+        // Simpan video ke storage
+        $path = $this->video->store('videos', 'public');
+
+        Edukasi::create([
+            'id_admin' => auth()->guard('admin')->user()->id_admin,
+            'judul' => $this->judul,
+            'tanggal_publikasi' => date('Y-m-d'),
+            'deskripsi' => $this->deskripsi,
+            'url_video' => $path,
+        ]);
+
+        $this->reset();
+    }
+
+    public function update() {
+
+        $this->edukasi->update([
+            'judul' => $this->judul,
+            'deskripsi' => $this->deskripsi,
+            'tanggal_publikasi' => date('Y-m-d'),
+        ]);
+
+        $this->reset();
+    }
+
+    public function fillFromModel(Edukasi $edukasi)
+    {
+        $this->edukasi = $edukasi;
+        $this->judul = $edukasi->judul;
+        $this->deskripsi = $edukasi->deskripsi;
+        $this->url_video = $edukasi->url_video;
+    }
+
+
+}
